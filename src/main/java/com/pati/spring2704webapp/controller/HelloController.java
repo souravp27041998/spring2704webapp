73c0ea12506e52f9f0f837dc0a0f343dcbc86dae
Sourav Pati1 <sourav.pati1@ibm.com>
2023-07-25 18:41:15 +0530

    added Hello controller

---
+++ src/main/java/com/pati/spring2704webapp/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.pati.spring2704webapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HelloController {
+@GetMapping("/hello")
+    public String sayHello(@RequestParam("name")String name){
+        return "Hello "+name;
+    }
+}
